955ae83d84e9aad46d30dd121bbb4076e1e1302f
nan0scho1ar <scorch267@gmail.com>
2022-05-25 05:15:49 +1000

    Extracted ansi-exec logic to seperate package.

--- ahoy.el
+++ ahoy.el
@@ -17,53 +17,17 @@
 ;; Run ahoy commands fron within Emacs.
 ;;
 ;;; Code:
-(require 'ansi-color)
+(require 'ansi-exec)
 
 (define-derived-mode ahoy-mode
-  fundamental-mode "Ahoy"
+  ansi-exec-mode "Ahoy"
   "Major mode for ahoy.")
-
-(defun ahoy-build-control-sequence-regexp (regexps)
-  "Build control sequence regexp from list of REGEXPS."
-  (mapconcat (lambda (regexp)
-               (concat "\\(?:" regexp "\\)"))
-             regexps "\\|"))
-
-(defvar ahoy-non-sgr-control-sequence-regexp
-      (ahoy-build-control-sequence-regexp
-       '(;; icon name escape sequences
-         "\033\\][0-2];.*?\007"
-         ;; non-SGR CSI escape sequences
-         "\033\\[\\??[0-9;]*[^0-9;m]"
-         ;; noop
-         "\012\033\\[2K\033\\[1F"
-         ;; tput sgr0
-         "\033(B"
-         ))
-     "Regexps which matches non-SGR control sequences.")
-
-(defun ahoy-filter-non-sgr-control-sequences-in-region (begin end)
-  (save-excursion
-    (goto-char begin)
-    (while
-        (re-search-forward ahoy-non-sgr-control-sequence-regexp end t)
-      (replace-match ""))))
 
 (defun ahoy-exec (cmd &optional args)
   "Run ahoy command CMD with arguments ARGS."
-  (let ((buffer (get-buffer-create (format "* Ahoy %s *" cmd))))
-    (when args (setq cmd (format "%s %s" cmd args)))
-    (with-current-buffer buffer (erase-buffer) (ahoy-mode))
-    (make-process :name "ahoy"
-                  :buffer buffer
-                  :command (list "bash" "-c" (format "TERM=xterm ahoy %s" cmd))
-                  :noquery t
-                  :sentinel (lambda (proc _event)
-                              (when (memq (process-status proc) '(exit signal))
-                                (with-current-buffer (process-buffer proc)
-                                  (ansi-color-apply-on-region (point-min) (point-max))
-                                  (ahoy-filter-non-sgr-control-sequences-in-region (point-min) (point-max)))
-                                (pop-to-buffer (process-buffer proc)))))))
+  (when args (setq cmd (format "%s %s" cmd args)))
+  (let ((buffname (format "* Ahoy %s *" cmd)))
+    (ansi-exec (format "ahoy %s" cmd) buffname buffname #'ahoy-mode)))
 
 (defun ahoy-cmds ()
   "Run ahoy in the current directory and return a list of possible commands."
